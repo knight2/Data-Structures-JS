952a46f36d662336df3c3adf79ad5867bcdba209
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-13 19:51:13 -0400

    stack commit

--- stack.js
+++ stack.js
@@ -1,1 +1,11 @@
-// Stacks
+//Stacks
+// Last-In-First-Out
+
+var homeworkStack = ["BIO12","HIS80","MAT122","PSY44"];
+homeworkStack.pop();
+homeworkStack.push('CS50');
+console.log(homeworkStack);
+
+//Stack Class
+//Create a stack class with push and pop method, as well as peek ,isEmpty,
+// and clear to the stack
