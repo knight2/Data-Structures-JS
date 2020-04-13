11e40556f0ab0afa13eb070e78975133ca4bfdb0
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-13 19:57:37 -0400

    added function Stack with several methods

--- stack.js
+++ stack.js
@@ -9,3 +9,26 @@ console.log(homeworkStack);
 //Stack Class
 //Create a stack class with push and pop method, as well as peek ,isEmpty,
 // and clear to the stack
+
+function Stack(){
+    var collection = [];
+    this.print = function(){
+        console.log(collection);
+    };
+    this.push = function(value){
+        return collection.push(value);
+    }
+    this.pop = function(){
+        return collection.pop();
+    }
+    this.peek = function(){
+        return collection[collection.length - 1];
+    }
+    this.isEmpty = function(){
+        return collection.length === 0;
+    };
+    this.clear = function(){
+        collection.length = 0;
+    };
+}
+
