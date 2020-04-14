6ca511286397dd0b3a78978e49b90e2f1083e837
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-14 06:34:42 -0400

    queue initial commit

---
+++ queue.js
@@ -0,0 +1,7 @@
+// Queue Class
+//Queues are similar to stacks, as a collection of elements
+// Unlike stacks, queues use First-In-First-Out
+//Elements added to the queue are pushed to the end
+//Only the element at the front is allowed to be removed
+//You can use an array but we want to limit control.
+
