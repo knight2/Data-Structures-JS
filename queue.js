db75eb59ee5ec1d321cadb6faf7f8f2bafe65bcd
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-14 06:59:51 -0400

    circular queue solution

--- queue.js
+++ queue.js
@@ -110,10 +110,26 @@ class CircularQueue{
     }
 
     enqueue(item){
+        if (this.queue[this.write] === null){
+            this.queue[this.write++] = item;
 
+            if (this.write > this.max){
+                this.write = 0;
+            }
+            return item;
+        }
+        return null;
     }
 
     dequeue(){
-        
+        if (this.queue[this.read] != null){
+            let item = this.queue[this.read];
+            this.queue[this.read++] = null;
+            if (this.read > this.max){
+                this.read = 0;
+            }
+            return item;
+        }
+        return null;
     }
 }
