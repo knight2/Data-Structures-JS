23f78cdaf3bfa936c0a680860418b19e53433047
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-15 09:08:46 -0400

    custom map with has valuesget remove add size

--- map.js
+++ map.js
@@ -7,4 +7,35 @@
 var Map = function(){
     this.collection = {};
 
+    this.add = function(key, value){
+        this.collection[key] = value;
+    }
+
+    this.remove = function(key, value){
+        delete this.collection[key];
+    };
+
+    this.get  = function(key){
+        return this.collection[key];
+    };
+
+    this.has = function(key){
+        return this.collection[key] !== undefined;
+    }
+
+    this.values = function(){
+        let ar = [];
+        for (let v in this.collection){
+            ar.push(this.collection[v]);
+        };
+        return ar;
+    }
+
+    this.size = function(){
+        return Object.keys(this.collection).length;
+    };
+
+    this.clear = function(){
+this.collection={}    }
+
 };
