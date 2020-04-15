02140fe06f6d2ac9e41a9f1aa2c3a6d43f18618c
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-15 09:13:37 -0400

    hash table initial commit

---
+++ hashTable.js
@@ -0,0 +1,13 @@
+var called = 0;
+var hash = string => {
+  called++;
+  var hashed = 0;
+  for (var i = 0; i < string.length; i++) {
+    hashed += string.charCodeAt(i);
+  }
+  return hashed;
+};
+var HashTable = function() {
+  this.collection = {};
+  
+};
