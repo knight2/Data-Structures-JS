4f295c4607e450266b554f076b6ab5af17a3c384
Knight2 <8440420+knight2@users.noreply.github.com>
2020-04-15 05:51:40 -0400

    map data structure initial commit

---
+++ map.js
@@ -0,0 +1,10 @@
+//Map Data Structures in JS
+
+//Maps are data structures that store key-value pairs.
+// In javascript these are available to us as objects.
+//Maps provide rapid lookup of stored items based on key values
+
+var Map = function(){
+    this.collection = {};
+
+};
